7189aa2df021bc63393499be51a3bc030bd3906c
Ira Matsuk <g3726866@gmail.com>
2019-12-14 19:08:21 +0300

    fix: use english words for the homework

--- Hw3.String/src/StringCompare.java
+++ Hw3.String/src/StringCompare.java
@@ -3,12 +3,12 @@ import java.util.Scanner;
 public class StringCompare {
 	public static void main (String[] args) {
 		Scanner scan = new Scanner(System.in);
-		System.out.println("Введите: Мама мыла раму");
+		System.out.println("Enter: Mom washed the frame");
 		String phrase = scan.nextLine().intern();
-		String str = "Мама мыла раму";
+		String str = "Mom washed the frame";
 		
 		System.out.println(str == phrase);
-		System.out.println(str == "Мама" + " мыла " + "раму");
+		System.out.println(str == "Mom" + " washed the " + "frame");
 		
 		scan.close();
 	}
