edb58954a28850cbfe3412516a2ec3254adc4324
Ira Matsuk <g3726866@gmail.com>
2019-12-14 18:57:08 +0300

    feat: compare pool and heap strings

---
+++ Hw3.String/src/StringCompare.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class StringCompare {
+	public static void main (String[] args) {
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Введите: Мама мыла раму");
+		String phrase = scan.nextLine().intern();
+		String str = "Мама мыла раму";
+		
+		System.out.println(str == phrase);
+		System.out.println(str == "Мама" + " мыла " + "раму");
+		
+		scan.close();
+	}
+}
