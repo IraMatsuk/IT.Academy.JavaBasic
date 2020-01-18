6c3d586202bf8655937edb64d8a2e6cf45192763
Ira Matsuk <g3726866@gmail.com>
2020-01-19 02:09:36 +0300

    refactor: create universal add and remove methods for all types of collections

--- Hw8.Collection/src/collection/CollectionMain.java
+++ Hw8.Collection/src/collection/CollectionMain.java
@@ -1,45 +1,23 @@
 package collection;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 public class CollectionMain {
 	public static void main(String[] args) {
-		List<Passport> listOfPassports = new ArrayList<>();
+		List<Passport> arrListOfPassports = new ArrayList<>();
+		List<Passport> linkListOfPassports = new LinkedList<>();
 		int numberOfPassports = 1_000_000;
-		long startTime = 0;
-		long endTime = 0;
-		long totalTime = 0;
-		for(int i = 0; i < numberOfPassports; i++) {
-			Passport passport = new Passport();
-			startTime = System.currentTimeMillis();
-			listOfPassports.add(passport);
-			endTime = System.currentTimeMillis();
-			totalTime += endTime - startTime;
-		}
-		System.out.println("Time to add all items: " + totalTime + " ms");
 		
-		// remove items from the end
-		for(int i = numberOfPassports - 1; i >= 0; i--) {
-			startTime = System.currentTimeMillis();
-			listOfPassports.remove(i);
-			endTime = System.currentTimeMillis();
-			totalTime += endTime - startTime;
-		}
-		System.out.println("Time to remove all items from the end: " + totalTime + " ms");
+		System.out.println("ArrayList statistic:");
+		GenericList.add(arrListOfPassports, numberOfPassports);
+		GenericList.remove(arrListOfPassports);
+		System.out.println("");
 		
-		for(int i = 0; i < numberOfPassports; i++) {
-			Passport passport = new Passport();
-			listOfPassports.add(passport);
-		}
-		
-		// remove items from the start
-		for(int i = 0; i < numberOfPassports; i++) {
-			startTime = System.currentTimeMillis();
-			listOfPassports.remove(0);
-			endTime = System.currentTimeMillis();
-			totalTime += endTime - startTime;
-		}
-		System.out.println("Time to remove all items from the start: " + totalTime + " ms");
+		System.out.println("LinkedList statistic:");
+		GenericList.add(linkListOfPassports, numberOfPassports);
+		GenericList.remove(linkListOfPassports);
+		System.out.println("");
 	}
 }
