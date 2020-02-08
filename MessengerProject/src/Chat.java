c643bdcfb5abf337acf1f73a16f94015afa4df8d
Ira Matsuk <g3726866@gmail.com>
2020-02-08 19:10:41 +0300

    feat: implementation of the chat methods

--- MessengerProject/src/Chat.java
+++ MessengerProject/src/Chat.java
@@ -1,21 +1,37 @@
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
+import java.util.Set;  
 
 public class Chat {
-	private final Set<User> users = new HashSet<>();
-	private final List<Message> messages = new ArrayList<>();
-	
-	public void addMessage(Message message) {
-			
-	}
-	
-	public void addMessage(Message[] message) {
-		
-	}
-	
-	public void addMessage(List<Message> message) {
-		
-	}
+    private final Set<User> users = new HashSet<>();
+    private final List<Message> messages = new ArrayList<>();
+
+    public Chat() {
+    }
+
+    public Chat(Set<User> users) { this.users.addAll(users);}
+
+   // @Override
+    public void addMessage(Message message) {
+        this.messages.add(message);
+    }
+
+   // @Override
+    public void addMessage(Message[] messages) {
+        if (messages != null) {
+            for (Message msg: messages) {
+                this.addMessage(msg);
+            }
+        }
+    }
+
+    //@Override
+    public void addMessage(List<Message> messages) {
+        if(messages != null) {
+            for (Message msg : messages) {
+                this.addMessage(msg);
+            }
+        }
+    }
 }
